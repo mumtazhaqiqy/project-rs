c05110a293a9afb7d208dd40ae4d6258a9d1e475
mumtazhaqiqy <mumtazhaqiqy@gmail.com>
2022-02-18 07:26:28 +0100

    update

--- app/Database/Migrations/2022-04-11-051557_Patients.php
+++ app/Database/Migrations/2022-04-11-051557_Patients.php
@@ -8,7 +8,21 @@ class Patients extends Migration
 {
     public function up()
     {
-        //
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'auto_increment' => true
+            ],
+            'noRekamMedis' => [
+                'type' => 'INT',
+                'constraint' => 50
+            ],
+            'namaLengkap' => [
+                'type' => 'VARCHAR',
+                'constraint' => 200
+            ],
+            
+        ])
     }
 
     public function down()
