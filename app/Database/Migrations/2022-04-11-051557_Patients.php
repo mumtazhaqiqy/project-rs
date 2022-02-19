593be96703a706c32a7d508e3fbaa0f9b5a4f768
mumtazhaqiqy <mumtazhaqiqy@gmail.com>
2022-02-19 07:30:08 +0100

    update

--- app/Database/Migrations/2022-04-11-051557_Patients.php
+++ app/Database/Migrations/2022-04-11-051557_Patients.php
@@ -14,19 +14,90 @@ class Patients extends Migration
                 'auto_increment' => true
             ],
             'noRekamMedis' => [
-                'type' => 'INT',
+                'type' => 'VARCHAR',
+                'constraint' => 50
+            ],
+            'alias' => [
+                'type' => 'VARCHAR',
                 'constraint' => 50
             ],
             'namaLengkap' => [
                 'type' => 'VARCHAR',
                 'constraint' => 200
             ],
-            
-        ])
+            'tempatLahir' => [
+                'type' => 'VARCHAR',
+                'constraint' => 50
+            ],
+            'tanggalLahir' => [
+                'type' => 'DATE',
+            ],
+            'jenisKelamin' => [
+                'type' => 'VARCHAR',
+                'constraint' => 50
+            ],
+            'agama' => [
+                'type' => 'VARCHAR',
+                'constraint' => 50
+            ],
+            'statusMenikah' => [
+                'type' => 'VARCHAR',
+                'constraint' => 50
+            ],
+            'pendidikanTerakhir' => [
+                'type' => 'INT',
+                'constraint' => 15
+            ],
+            'golonganDarah' => [
+                'type' => 'VARCHAR',
+                'constraint' => 10
+            ],
+            'pekerjaanSekarang' => [
+                'type' => 'INT',
+                'constraint' => 10
+            ],
+            'alamatTinggal' => [
+                'type' => 'TEXT',
+                'null' => true
+            ], 
+            'noHandphone' => [
+                'type' => 'VARCHAR',
+                'constraint' => 20
+            ],
+            'ktp' => [
+                'type' => 'VARCHAR',
+                'constraint' => 30
+            ],
+            'bpjs' => [
+                'type' => 'VARCHAR',
+                'constraint' => 30
+            ],
+            'ayah' => [
+                'type' => 'VARCHAR',
+                'constraint' => 200
+            ],
+            'ibu' => [
+                'type' => 'VARCHAR',
+                'constraint' => 200
+            ],
+            'pasangan' => [
+                'type' => 'VARCHAR',
+                'constraint' => 200
+            ],
+            'uploadFile' => [
+                'type' => 'TEXT',
+                'null' => true
+            ],
+            'created_at datetime default current_timestamp',
+            'updated_at datetime default current_timestamp on update current_timestamp',
+        ]); 
+
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('patients');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('patients');
     }
 }
