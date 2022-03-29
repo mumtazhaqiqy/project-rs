6f1161b0c97d0ddbdf1da4cb6df3ba3969e0e2dd
mumtazhaqiqy <mumtazhaqiqy@gmail.com>
2022-03-30 00:19:38 +0200

    add users migration

---
+++ app/Database/Migrations/2022-03-29-221049_Users.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+use PHPUnit\Framework\Constraint\Constraint;
+
+class Users extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT', 
+                'auto_increment' => true
+            ],
+            'name' => [
+                'type' => 'VARCHAR',
+                'constraint' => '50'
+            ], 
+            'email' => [
+                'type' => 'VARCHAR',
+                'constraint' => '50'
+            ],
+            'password' => [
+                'type' => 'VARCHAR',
+                'constraint' => '20'
+            ],
+            'phone' => [
+                'type' => 'VARCHAR',
+                'constraint' => '15'
+            ], 
+            'address' => [
+                'type' => 'TEXT',
+            ],
+            'created_at datetime default current_timestamp',
+            'updated_at datetime default cureent_timestapm on update current_timestamp'
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('users');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('users');
+    }
+}
